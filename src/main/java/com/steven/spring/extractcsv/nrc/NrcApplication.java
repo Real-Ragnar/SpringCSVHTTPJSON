4434125503c191988591058e3063946827363045
Steven Souksanh <steven.souksanh@gmail.com>
2024-04-02 00:38:45 -0400

    Readme

--- src/main/java/com/steven/spring/extractcsv/nrc/NrcApplication.java
+++ src/main/java/com/steven/spring/extractcsv/nrc/NrcApplication.java
@@ -3,7 +3,6 @@ package com.steven.spring.extractcsv.nrc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -16,60 +15,55 @@ public class NrcApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NrcApplication.class, args);
-		// Endpoint URL where the CSV file is hosted
-		String endpointUrl = "http://localhost:8080/download";
+
+		// Endpoint URL to retrieve the CSV data
+		String endpointUrlCSV = "http://localhost:8080/download";
 
 		// Optional query parameters
-		int startRow = 2; // Example: Start downloading from the second row
-		int count = 10;   // Example: Download only 10 rows
+		int startRow = 2; // Start downloading from the second row. 0th row would be the column titles.
+		//Setting start to anything >0 may remove column titles from appearing in JSON response
+		int count = 5;   // Download only 5 rows
 
 		// Build the URL with query parameters
-		StringBuilder urlBuilder = new StringBuilder(endpointUrl);
+		StringBuilder urlBuilder = new StringBuilder(endpointUrlCSV);
 		urlBuilder.append("?start=").append(startRow);
 		urlBuilder.append("&count=").append(count);
 
-		String url = urlBuilder.toString();
+		String urlendpointUrlCSVString = urlBuilder.toString();
 
-		// Perform the HTTP GET request
+		System.out.println("Perform the HTTP GET request");
 		try {
-			HttpClient client = HttpClient.newHttpClient();
-			HttpRequest request = HttpRequest.newBuilder()
-					.uri(new URI(url))
+			HttpClient httpClient = HttpClient.newHttpClient();
+			HttpRequest httpRequest = HttpRequest.newBuilder()
+					.uri(new URI(urlendpointUrlCSVString))
 					.build();
 
-			HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+			HttpResponse<String> httpResponse = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
 
-			// Check if the request was successful
-			if (response.statusCode() == 200) {
-				// Print the CSV content
-				System.out.println(response.body());
+			// If response was successful, print out the CSV response
+			if (httpResponse.statusCode() == 200) {
+				System.out.println(httpResponse.body());
 			} else {
-				System.err.println("Failed to download CSV. Status code: " + response.statusCode());
+				System.err.println("Error. Status code: " + httpResponse.statusCode());
 			}
 		} catch (URISyntaxException | IOException | InterruptedException e) {
 			e.printStackTrace();
 		}
 
-		System.out.println("Break================");
-		
-		// Perform the HTTP GET request with "Accept" header set to "application/json"
+		System.out.println("Perform the HTTP GET request with \"Accept\" header set to \"application/json\"");
 		try {
-			HttpClient client = HttpClient.newHttpClient();
-			HttpRequest request = HttpRequest.newBuilder()
-					.uri(new URI(url))
+			HttpClient httpClient = HttpClient.newHttpClient();
+			HttpRequest httpRequest = HttpRequest.newBuilder()
+					.uri(new URI(urlendpointUrlCSVString))
 					.header("Accept", "application/json")
 					.build();
 
-			HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+			HttpResponse<String> httpResponse = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
 
-			// Print the response body
 			System.out.println("Response Body:");
-			System.out.println(response.body());
+			System.out.println(httpResponse.body());
 		} catch (URISyntaxException | IOException | InterruptedException e) {
 			e.printStackTrace();
 		}
-
-
     }
-
 }
--- src/main/java/com/steven/spring/extractcsv/nrc/rest/NRCRestController.java
+++ src/main/java/com/steven/spring/extractcsv/nrc/rest/NRCRestController.java
@@ -6,24 +6,24 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.stream.Collectors;
 
 
 @RestController
 public class NRCRestController {
 
-    private static final String CSV_FILE_PATH = "55262.csv";
+    private static final String CSV_FILE_PATH = "55262d.csv";
 
     @GetMapping(value = "/download")
     public ResponseEntity<?> downloadCSV(
@@ -58,7 +58,7 @@ public class NRCRestController {
 
             // Read CSV lines and collect them into a single string
             String csvContent = reader.lines()
-                    .limit(count != null ? count : Long.MAX_VALUE) // Limit the number of lines if count parameter is provided
+                    .limit(count != null ? count : Long.MAX_VALUE) // Limit number of lines to the specified count value, otherwise set to largest value
                     .collect(Collectors.joining("\n"));
 
             return csvContent;
@@ -71,27 +71,41 @@ public class NRCRestController {
     private String generateJsonResponse(ClassPathResource resource, Integer start, Integer count) {
         // Parse CSV content and convert it to JSON
         try (CSVReader reader = new CSVReader(new InputStreamReader(resource.getInputStream()))) {
+            // Read the first row which is the header row to get column names
+            String[] headers = reader.readNext();
+            if (headers == null) {
+                // Return an empty JSON object if the CSV file is empty
+                return new JSONObject().toString();
+            }
             // Skip lines if start parameter is provided
             for (int i = 0; i < start; i++) {
-                reader.readNext();
+                if (reader.readNext() == null) {
+                    // Return an empty JSON object if start parameter exceeds the number of rows
+                    return new JSONObject().toString();
+                }
             }
 
-            // Read CSV lines and convert them to JSON
-            JSONArray jsonArray = new JSONArray();
-            String[] line;
+            // Read CSV lines and convert them to JSON Array to be displayed
+            JSONArray csvRowsJsonArray = new JSONArray();
+            String[] csvRowLine;
             int lineCount = 0;
-            while ((line = reader.readNext()) != null && (count == null || lineCount < count)) {
-                JSONObject jsonObject = new JSONObject();
-                for (int i = 0; i < line.length; i++) {
-                    jsonObject.put("column" + (i + 1), line[i]);
+            while ((csvRowLine = reader.readNext()) != null && (count == null || lineCount < count)) {
+                JSONObject csvRowJsonObject = new JSONObject(); // Created for each row, and each data value is assigned to its corresponding column title.
+                for (int i = 0; i < headers.length; i++) {
+                    // Format each row entry to contain the column title
+                    csvRowJsonObject.put(headers[i], csvRowLine[i]);
                 }
-                jsonArray.put(jsonObject);
+                csvRowsJsonArray.put(csvRowJsonObject);
                 lineCount++;
             }
 
-            return jsonArray.toString();
+            // Create a JSON object to hold both column titles and data
+            JSONObject jsonResult = new JSONObject();
+            jsonResult.put("Observation Data", csvRowsJsonArray); // Add CSV data
+
+            return jsonResult.toString(2); // Indent JSON for readability
         } catch (IOException e) {
-            throw new RuntimeException("Error occurred while reading CSV file", e);
+            throw new RuntimeException("Error while reading CSV file", e);
         } catch (CsvValidationException e) {
             throw new RuntimeException(e);
         } catch (JSONException e) {
